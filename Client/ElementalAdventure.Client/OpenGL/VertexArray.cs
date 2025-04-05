0d4d52e4bede7c9c4a8fc1aa8781a4a2f0b066ff
siva1danil <siva1danil@ya.ru>
2025-04-06 03:28:46 +0700

    Template-based VertexArray

--- Client/ElementalAdventure.Client/OpenGL/VertexArray.cs
+++ Client/ElementalAdventure.Client/OpenGL/VertexArray.cs
@@ -1,38 +1,41 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
 using OpenTK.Graphics.OpenGL4;
 
 namespace ElementalAdventure.Client.OpenGL;
 
-public class VertexArray : IDisposable {
+public class VertexArray<T> : IDisposable where T : struct {
     private readonly int _vao = -1;
     private readonly int _vbo = -1;
 
+    private readonly int _stride;
+
     public int Id => _vao;
 
-    public VertexArray(int[] attributes) {
+    public VertexArray() {
         _vao = GL.GenVertexArray();
         _vbo = GL.GenBuffer();
         GL.BindVertexArray(_vao);
         GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
         GL.BufferData(BufferTarget.ArrayBuffer, 0, IntPtr.Zero, BufferUsageHint.DynamicDraw);
 
-        int stride = 0;
-        foreach (int attribute in attributes)
-            stride += attribute * sizeof(float);
-
-        int offset = 0;
-        for (int i = 0; i < attributes.Length; i++) {
-            GL.EnableVertexAttribArray(i);
-            GL.VertexAttribPointer(i, attributes[i], VertexAttribPointerType.Float, false, stride, offset);
-            offset += attributes[i] * sizeof(float);
+        _stride = Marshal.SizeOf<T>();
+        int index = 0;
+        foreach (FieldInfo field in typeof(T).GetFields()) {
+            int size = Marshal.SizeOf(field.FieldType) / sizeof(float);
+            int offset = Marshal.OffsetOf<T>(field.Name).ToInt32();
+            GL.VertexAttribPointer(index, size, VertexAttribPointerType.Float, false, _stride, offset);
+            GL.EnableVertexAttribArray(index);
         }
 
         GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
         GL.BindVertexArray(0);
     }
 
-    public void SetData(float[] data) {
+    public void SetData(T[] data, BufferUsageHint usage = BufferUsageHint.DynamicDraw) {
         GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
-        GL.BufferData(BufferTarget.ArrayBuffer, data.Length * sizeof(float), data, BufferUsageHint.DynamicDraw);
+        GL.BufferData(BufferTarget.ArrayBuffer, data.Length * _stride, data, usage);
         GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
     }
 
--- Client/ElementalAdventure.Client/Scenes/ExampleScene.cs
+++ Client/ElementalAdventure.Client/Scenes/ExampleScene.cs
@@ -1,3 +1,6 @@
+using System.Numerics;
+using System.Runtime.InteropServices;
+
 using ElementalAdventure.Client.OpenGL;
 using ElementalAdventure.Client.Resources;
 
@@ -8,13 +11,13 @@ namespace ElementalAdventure.Client.Scenes;
 public class ExampleScene : IScene {
     private readonly ResourceRegistry _resourceRegistry;
 
-    private readonly VertexArray _vao;
+    private readonly VertexArray<Vertex> _vao;
 
     public ExampleScene(ResourceRegistry resourceRegistry) {
         _resourceRegistry = resourceRegistry;
 
-        _vao = new VertexArray([3]);
-        _vao.SetData([-1, -1, 0, 1, -1, 0, 1, 1, 0, -1, 1, 0]);
+        _vao = new VertexArray<Vertex>();
+        _vao.SetData([new(new(-1, -1, 0)), new(new(1, -1, 0)), new(new(1, 1, 0)), new(new(-1, 1, 0))]);
     }
 
     public void Update() {
@@ -24,12 +27,17 @@ public class ExampleScene : IScene {
     public void Render() {
         GL.UseProgram(_resourceRegistry.GetShader("default").Id);
         GL.BindVertexArray(_vao.Id);
-        GL.DrawArrays(PrimitiveType.Triangles, 0, 4);
+        GL.DrawArrays(PrimitiveType.TriangleStrip, 0, 4);
         GL.BindVertexArray(0);
         GL.UseProgram(0);
     }
 
     public void Dispose() {
         _vao.Dispose();
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct Vertex(Vector3 position) {
+        public Vector3 Position = position;
     }
 }
