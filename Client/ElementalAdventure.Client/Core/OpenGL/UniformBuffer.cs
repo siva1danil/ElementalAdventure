abb9fb53e16228aa88d06520260b011a93c42f35
siva1danil <siva1danil@ya.ru>
2025-04-15 19:50:45 +0700

    Implement UniformBuffer

---
+++ Client/ElementalAdventure.Client/Core/OpenGL/UniformBuffer.cs
@@ -0,0 +1,24 @@
+using System.Runtime.InteropServices;
+
+using OpenTK.Graphics.OpenGL4;
+
+namespace ElementalAdventure.Client.Core.OpenGL;
+
+public class UniformBuffer<T> where T : unmanaged {
+    private readonly int _id;
+
+    public int Id => _id;
+
+    public UniformBuffer() {
+        _id = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.UniformBuffer, _id);
+        GL.BufferData(BufferTarget.UniformBuffer, Marshal.SizeOf<T>(), IntPtr.Zero, BufferUsageHint.DynamicDraw);
+        GL.BindBuffer(BufferTarget.UniformBuffer, 0);
+    }
+
+    public void SetData(ref T data) {
+        GL.BindBuffer(BufferTarget.UniformBuffer, _id);
+        GL.BufferSubData(BufferTarget.UniformBuffer, 0, Marshal.SizeOf<T>(), ref data);
+        GL.BindBuffer(BufferTarget.UniformBuffer, 0);
+    }
+}
