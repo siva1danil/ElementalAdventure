b9ec28d42b77cb2f56ea78d4681539e99960d201
siva1danil <siva1danil@ya.ru>
2025-04-21 02:49:13 +0700

    Optimize Enemy target search

--- Client/ElementalAdventure.Client/Game/Logic/Component/Behaviour/EnemyBehaviourComponent.cs
+++ Client/ElementalAdventure.Client/Game/Logic/Component/Behaviour/EnemyBehaviourComponent.cs
@@ -1,5 +1,6 @@
 using ElementalAdventure.Client.Game.Data;
 using ElementalAdventure.Client.Game.Logic.GameObject;
+using ElementalAdventure.Client.Game.Utils;
 
 using OpenTK.Mathematics;
 
@@ -8,24 +9,21 @@ namespace ElementalAdventure.Client.Game.Logic.Component.Behaviour;
 public class EnemyBehaviourComponent : IBehavourComponent {
     private readonly EnemyType _enemyType;
 
+    private readonly WeakReference<Entity?> _target = new(null);
     private bool _facingRight = true;
+    private int _searchCounter = 0;
 
     public EnemyBehaviourComponent(EnemyType enemyType) {
         _enemyType = enemyType;
     }
 
     public void Update(GameWorld world, Entity entity) {
-        // Follow player
-        Entity? player = null;
-        foreach (Entity e in world.Entities)
-            if (e.LivingDataComponent?.TargetableByEnemies ?? false)
-                player = e;
-        if (player != null) {
-            Vector2 direction = player.PositionDataComponent.Position - entity.PositionDataComponent.Position;
-            entity.PositionDataComponent.Velocity = direction.LengthSquared > 0.0f ? direction.Normalized() : Vector2.Zero;
-        } else {
-            entity.PositionDataComponent.Velocity = Vector2.Zero;
-        }
+        // Find target
+        if (_searchCounter == 0 && !_target.TryGetTarget(out Entity? _)) FindTarget(world, entity);
+        _searchCounter = (_searchCounter + 1) % 20;
+
+        // Follow target
+        entity.PositionDataComponent.Velocity = (_target.TryGetTarget(out Entity? target) && target != null) ? (target.PositionDataComponent.Position - entity.PositionDataComponent.Position).NormalizedOrZero() : Vector2.Zero;
 
         // Tick movement
         entity.PositionDataComponent.Position += entity.PositionDataComponent.Velocity * entity.LivingDataComponent!.MovementSpeed;
@@ -45,5 +43,11 @@ public class EnemyBehaviourComponent : IBehavourComponent {
             entity.TextureDataComponent.TextureAtlas = _enemyType.TextureAtlas;
             entity.TextureDataComponent.Texture = _facingRight ? _enemyType.TextureIdleRight : _enemyType.TextureIdleLeft;
         }
+    }
+
+    private void FindTarget(GameWorld world, Entity entity) {
+        foreach (Entity e in world.Entities)
+            if (e.LivingDataComponent?.TargetableByEnemies ?? false)
+                _target.SetTarget(e);
     }
 }
