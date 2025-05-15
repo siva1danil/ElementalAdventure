4ebd4acce527e8e0a49c3ff1055fbfae23d886b6
siva1danil <siva1danil@ya.ru>
2025-05-16 02:55:36 +0700

    LinearLayout gravity support

--- Client/ElementalAdventure.Client/Game/UI/ViewGroup/LinearLayout.cs
+++ Client/ElementalAdventure.Client/Game/UI/ViewGroup/LinearLayout.cs
@@ -7,11 +7,14 @@ namespace ElementalAdventure.Client.Game.UI.ViewGroups;
 
 public class LinearLayout : ViewGroupBase {
     private OrientationType _orientation;
+    private GravityType _gravity;
 
     public OrientationType Orientation { get => _orientation; set => _orientation = value; }
+    public GravityType Gravity { get => _gravity; set => _gravity = value; }
 
     public LinearLayout() {
         _orientation = OrientationType.Horizontal;
+        _gravity = GravityType.Start;
     }
 
     public override void Measure(Vector2 available) {
@@ -26,9 +29,16 @@ public class LinearLayout : ViewGroupBase {
     public override void Layout(float depth = 0.0f, float step = 0.0f) {
         Vector2 position = _computedPosition.Xy;
         foreach (IView view in _views) {
+            if (_gravity == GravityType.Center)
+                position = _orientation == OrientationType.Horizontal
+                    ? new Vector2(position.X, position.Y + (_computedSize.Y - view.ComputedSize.Y) / 2.0f)
+                    : new Vector2(position.X + (_computedSize.X - view.ComputedSize.X) / 2.0f, position.Y);
+            else if (_gravity == GravityType.End)
+                position = _orientation == OrientationType.Horizontal
+                    ? new Vector2(position.X, position.Y + _computedSize.Y - view.ComputedSize.Y)
+                    : new Vector2(position.X + _computedSize.X - view.ComputedSize.X, position.Y);
             view.ComputedPosition = new Vector3(position.X, position.Y, depth);
-            if (_orientation == OrientationType.Horizontal) position.X += view.ComputedSize.X;
-            else position.Y += view.ComputedSize.Y;
+            position += _orientation == OrientationType.Horizontal ? new Vector2(view.ComputedSize.X, 0.0f) : new Vector2(0.0f, view.ComputedSize.Y);
 
             if (view is IViewGroup group)
                 group.Layout(depth + step, step);
@@ -41,5 +51,6 @@ public class LinearLayout : ViewGroupBase {
     }
 
     public enum OrientationType { Horizontal, Vertical }
+    public enum GravityType { Start, Center, End }
     public class LayoutParams : IViewGroup.ILayoutParams { }
 }
