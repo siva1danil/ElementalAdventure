3eae88f5feceb4b9de2c21ae4735b889829099b8
siva1danil <siva1danil@ya.ru>
2025-05-23 02:20:43 +0700

    Collision logic

--- Client/ElementalAdventure.Client/Game/WorldLogic/Component/Behaviour/EnemyBehaviourComponent.cs
+++ Client/ElementalAdventure.Client/Game/WorldLogic/Component/Behaviour/EnemyBehaviourComponent.cs
@@ -27,7 +27,40 @@ public class EnemyBehaviourComponent : IBehavourComponent {
         entity.PositionDataComponent.Velocity = movement.LengthSquared > 1.0f ? movement.NormalizedOrZero() : movement;
 
         // Tick movement
-        entity.PositionDataComponent.Position += entity.PositionDataComponent.Velocity * entity.LivingDataComponent!.MovementSpeed;
+        Vector2 position = entity.PositionDataComponent.Position;
+        Vector2 velocity = entity.PositionDataComponent.Velocity * entity.LivingDataComponent!.MovementSpeed;
+        if (entity.HitboxDataComponent != null) {
+            Vector2 newPositionX = new(position.X + velocity.X, position.Y);
+            Box2 hitboxX = new(entity.HitboxDataComponent.Box.Min + newPositionX, entity.HitboxDataComponent.Box.Max + newPositionX);
+            Box2 collider = default;
+            foreach (Box2 wall in world.Tilemap.Walls) {
+                if (wall.Intersects(hitboxX)) {
+                    collider = wall;
+                    break;
+                }
+            }
+            if (collider == default)
+                position.X += velocity.X;
+            else if (velocity.X != 0)
+                position.X = velocity.X > 0 ? collider.Min.X - entity.HitboxDataComponent.Box.Max.X - 0.01f : collider.Max.X + (0.0f - entity.HitboxDataComponent.Box.Min.X) + 0.01f;
+
+            Vector2 newPositionY = new(position.X, position.Y + velocity.Y);
+            Box2 hitboxY = new(entity.HitboxDataComponent.Box.Min + newPositionY, entity.HitboxDataComponent.Box.Max + newPositionY);
+            Box2 colliderY = default;
+            foreach (Box2 wall in world.Tilemap.Walls) {
+                if (wall.Intersects(hitboxY)) {
+                    colliderY = wall;
+                    break;
+                }
+            }
+            if (colliderY == default)
+                position.Y += velocity.Y;
+            else if (velocity.Y != 0)
+                position.Y = velocity.Y > 0 ? colliderY.Min.Y - entity.HitboxDataComponent.Box.Max.Y - 0.01f : colliderY.Max.Y + (0.0f - entity.HitboxDataComponent.Box.Min.Y) + 0.01f;
+        } else {
+            position += new Vector2(velocity.X, velocity.Y);
+        }
+        entity.PositionDataComponent.Position = position;
 
         // Update Z
         entity.PositionDataComponent.Z = world.Tilemap.GetNormalizedDepth(world.Tilemap.Midground, entity.PositionDataComponent.Position.Y, _enemyType.DepthLayerOffset, _enemyType.DepthHeightOffset);
--- Client/ElementalAdventure.Client/Game/WorldLogic/Component/Behaviour/PlayerBehaviourComponent.cs
+++ Client/ElementalAdventure.Client/Game/WorldLogic/Component/Behaviour/PlayerBehaviourComponent.cs
@@ -18,15 +18,38 @@ public class PlayerBehaviourComponent : IBehavourComponent {
         entity.PositionDataComponent.Velocity = world.Input;
 
         // Tick movement
-        Vector2 position = entity.PositionDataComponent.Position + entity.PositionDataComponent.Velocity * entity.LivingDataComponent!.MovementSpeed;
+        Vector2 position = entity.PositionDataComponent.Position;
+        Vector2 velocity = entity.PositionDataComponent.Velocity * entity.LivingDataComponent!.MovementSpeed;
         if (entity.HitboxDataComponent != null) {
-            Box2 hitbox = new Box2(entity.HitboxDataComponent.Box.Min + position, entity.HitboxDataComponent.Box.Max + position);
+            Vector2 newPositionX = new(position.X + velocity.X, position.Y);
+            Box2 hitboxX = new(entity.HitboxDataComponent.Box.Min + newPositionX, entity.HitboxDataComponent.Box.Max + newPositionX);
+            Box2 collider = default;
             foreach (Box2 wall in world.Tilemap.Walls) {
-                if (wall.Intersects(hitbox)) {
-                    position -= entity.PositionDataComponent.Velocity * entity.LivingDataComponent!.MovementSpeed;
+                if (wall.Intersects(hitboxX)) {
+                    collider = wall;
                     break;
                 }
             }
+            if (collider == default)
+                position.X += velocity.X;
+            else if (velocity.X != 0)
+                position.X = velocity.X > 0 ? collider.Min.X - entity.HitboxDataComponent.Box.Max.X - 0.01f : collider.Max.X + (0.0f - entity.HitboxDataComponent.Box.Min.X) + 0.01f;
+
+            Vector2 newPositionY = new(position.X, position.Y + velocity.Y);
+            Box2 hitboxY = new(entity.HitboxDataComponent.Box.Min + newPositionY, entity.HitboxDataComponent.Box.Max + newPositionY);
+            Box2 colliderY = default;
+            foreach (Box2 wall in world.Tilemap.Walls) {
+                if (wall.Intersects(hitboxY)) {
+                    colliderY = wall;
+                    break;
+                }
+            }
+            if (colliderY == default)
+                position.Y += velocity.Y;
+            else if (velocity.Y != 0)
+                position.Y = velocity.Y > 0 ? colliderY.Min.Y - entity.HitboxDataComponent.Box.Max.Y - 0.01f : colliderY.Max.Y + (0.0f - entity.HitboxDataComponent.Box.Min.Y) + 0.01f;
+        } else {
+            position += new Vector2(velocity.X, velocity.Y);
         }
         entity.PositionDataComponent.Position = position;
 
